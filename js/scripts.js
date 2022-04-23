b3b509cd8f49c9d92f4466f966869ed3f2ea40ee
Sue Roberts <suerobertstx@yahoo.com>
2022-04-23 00:51:53 -0500

    Update and fix outputSong.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,22 @@
-function neighborhood(num) {
-  let numArray = [];
+function neighborhood(num){
   let finalSongArray = [];
   if (Number.parseInt(num) < 0 || num == "") {
-    let negNumber = ("please enter positive number!");
-    finalSongArray =  negNumber;
-  } else if(Number.parseInt(num) >= 0){
+    let negNumber = ("please enter a positive number!");
+    finalSongArray = negNumber;
+  } else if(Number.parseInt(num) >= 0) {
     for(let i = 0; i <= num; i++) {
-    let updatedSongArray = i.toString()
-    if(updatedSongArray.includes("3")) {
-      finalSongArray.push(" Won't you be my Neighbor?");
-    } else if(updatedSongArray.includes("2")) {
-      finalSongArray.push(" Boop!");
-    } else if(updatedSongArray.includes("1")) {
-      finalSongArray.push(" Beep!");
-    } else {
-      finalSongArray.push(updatedSongArray);
-    }
+    let updatedSongArray = i.toString();
+      if(updatedSongArray.includes("3")) {
+        finalSongArray.push(" Won't you be my Neighbor?");
+      } else if(updatedSongArray.includes("2")) {
+        finalSongArray.push(" Boop!");
+      } else if(updatedSongArray.includes("1")) {
+        finalSongArray.push(" Beep!");
+      } else{
+        finalSongArray.push(updatedSongArray);
+      }
     } 
-  } return finalSongArray;
+  } return finalSongArray ;
 }
 
 $(document).ready(function () {
